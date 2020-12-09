d41ff1895184027cfec433a9be91bb99fd29af14
aquilas91 <agaq2020@gmail.com>
2020-12-09 13:14:15 -0600

    ok

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const path = require('path')
 const { SERVER_PORT } = process.env;
 const app = express();
 
+app.use(express.static(__dirname + '/../build'));
+
 app.listen(SERVER_PORT, () => {
     console.log(`listening on ${SERVER_PORT}`);
 })
